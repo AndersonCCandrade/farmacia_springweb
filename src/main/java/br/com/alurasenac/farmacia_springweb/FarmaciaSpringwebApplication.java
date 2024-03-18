e52430c522ec2dee9a451ad2d9e1afe8b61e448f
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-03-18 17:55:34 -0300

    Tarefa 1 - Setup inicial do Projeto

---
+++ src/main/java/br/com/alurasenac/farmacia_springweb/FarmaciaSpringwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurasenac.farmacia_springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaSpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaSpringwebApplication.class, args);
+	}
+
+}
