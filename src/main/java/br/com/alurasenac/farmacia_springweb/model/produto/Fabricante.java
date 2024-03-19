43b41b68533a5a84bc53d8438d1c085a868953ee
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-03-18 22:45:21 -0300

    Tarefa 4 - Criar a Classe de Produto e Fabricante

---
+++ src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Fabricante.java
@@ -0,0 +1,9 @@
+package br.com.alurasenac.farmacia_springweb.model.produto;
+
+import java.util.List;
+
+public class Fabricante {
+    private int id;
+    private String nome;
+
+}
---
+++ src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Produto.java
@@ -0,0 +1,9 @@
+package br.com.alurasenac.farmacia_springweb.model.produto;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private Fabricante fabricante;
+}
