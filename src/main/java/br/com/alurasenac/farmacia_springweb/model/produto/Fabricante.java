512b0a3143443ef2e8cb08383bd040f1cec1e44a
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-03-21 19:54:35 -0300

    Tarefa 5 - Mapear as Entidades para Tabelas

--- src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Fabricante.java
+++ src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Fabricante.java
@@ -1,9 +1,21 @@
 package br.com.alurasenac.farmacia_springweb.model.produto;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Entity(name = "Fabricante")
+@Table(name = "fabricantes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Fabricante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
-
 }
--- src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Produto.java
+++ src/main/java/br/com/alurasenac/farmacia_springweb/model/produto/Produto.java
@@ -1,9 +1,24 @@
 package br.com.alurasenac.farmacia_springweb.model.produto;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Produto")
+@Table(name = "produtos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private String descricao;
     private double preco;
+    @ManyToOne
     private Fabricante fabricante;
 }
